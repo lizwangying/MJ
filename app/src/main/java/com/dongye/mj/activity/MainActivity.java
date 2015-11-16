9e22af63ff204ccd1e4b7332f2f3cef490dc8660
wangying <lizwangying@icloud.com>
2015-11-16 16:38:21 +0800

    没写完，我就试试能不能提交

--- app/src/main/java/com/dongye/mj/activity/MainActivity.java
+++ app/src/main/java/com/dongye/mj/activity/MainActivity.java
@@ -2,14 +2,30 @@ package com.dongye.mj.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.dongye.mj.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.alertdialog)
+    Button alertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.alertdialog) void popAlert(){
+
     }
 }
+
