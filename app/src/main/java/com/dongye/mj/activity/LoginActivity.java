63d1e3eb373fd9d1a0f7d2bb491542fdaefbc6b9
Liz <lizwangying@icloud.com>
2016-04-08 10:03:33 +0800

    green spots!!!

--- app/src/main/java/com/dongye/mj/activity/LoginActivity.java
+++ app/src/main/java/com/dongye/mj/activity/LoginActivity.java
@@ -16,5 +16,9 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+
+
     }
+
 }
